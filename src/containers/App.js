6455833cbdacbcd4ef40ff3f518df212d8199800
infosalim <salimh5757@gmail.com>
2019-02-06 23:58:28 +0600

    changed App structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -75,7 +75,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <h1>Salim Hossain</h1>
-        <p className={AssignedClasses.join(' ')}>A Front End Web Developer</p>
+        <p className={AssignedClasses.join(' ')}>Front End Web Developer</p>
         <button
           className={btnClass}
           onClick={this.togglePersonHandler}>Toggle Person</button>
