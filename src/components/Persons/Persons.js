bac4ce57fea4e0d599e1941db52c7d3e1f1b49fb
infosalim <salimh5757@gmail.com>
2019-02-07 02:11:50 +0600

    change person

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,7 +4,7 @@ import Person from './Person/Person';
 
 const persons = (props) => props.persons.map((person, index) => {
     return <Person
-        click={() => props.clicked(index)}
+        clicked={() => props.clicked(index)}
         name={person.name}
         age={person.age}
         key={person.id}
